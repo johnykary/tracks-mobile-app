13ebe821618303e4eab0b6dbc39faa6a961d8629
giannis.karystinos <giannis.karystinos@gugroup.com>
2021-07-03 21:07:02 +0300

    location context

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,35 +1,31 @@
-import React from "react";
-import { StyleSheet } from "react-native";
+import React, { useContext } from "react";
+import { StyleSheet, ActivityIndicator } from "react-native";
 import MapView, { Polyline } from "react-native-maps";
+import { Context as LocationContext } from "../context/LocationContext";
 
 const Map = () => {
-  let points = [];
-  for (let i = 0; i < 20; i++) {
-      if(i%2 === 0){
-        points.push({
-            latitude: 37.33233 + 1 * 0.01,
-            longitude: -122.03121 + 1 * 0.01,
-          });
-      }else{
-        points.push({
-            latitude: 37.33233 - 1 * 0.02,
-            longitude: -122.03121 + 1 * 0.02,
-          });
-      }
-    
+  const {
+    state: { currentLocation },
+  } = useContext(LocationContext);
+  if (!currentLocation) {
+    return <ActivityIndicator size="large" style={{ marginTop: 200 }} />;
   }
   return (
     <MapView
       style={styles.map}
       initialRegion={{
-        latitude: 37.33233,
-        longitude: -122.03121,
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
         latitudeDelta: 0.01,
         longitudeDelta: 0.01,
       }}
-    >
-      <Polyline coordinates={points} />
-    </MapView>
+      region={{
+        latitude: currentLocation.coords.latitude,
+        longitude: currentLocation.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      }}
+    ></MapView>
   );
 };
 
--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,18 +1,68 @@
-import React from "react";
+import "../_mockLocation";
+import React, { useEffect, useState, useContext } from "react";
 import { StyleSheet } from "react-native";
 import { Text } from "react-native-elements";
-import {SafeAreaView} from 'react-navigation';
+import { SafeAreaView } from "react-navigation";
 import Map from "../components/Map";
+import {
+  requestForegroundPermissionsAsync,
+  watchPositionAsync,
+  Accuracy,
+} from "expo-location";
+import { Context as LocationContext } from "../context/LocationContext";
 
 const TrackCreateScreen = () => {
+  const { addLocation } = useContext(LocationContext);
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestForegroundPermissionsAsync();
+      if (!granted) {
+        throw new Error("Location permission not granted");
+      }
+      await watchPositionAsync(
+        {
+          accuracy: Accuracy.BestForNavigation,
+          timeInterval: 1000,
+          distanceInterval: 10,
+        },
+        (location) => {
+          addLocation(location);
+        }
+      );
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
-      <SafeAreaView forceInset={{top:'always'}}>
-          <Text h3> Create a Track</Text>
-          <Map/>
-      </SafeAreaView>
+    <SafeAreaView forceInset={{ top: "always" }}>
+      <Text h3> Create a Track</Text>
+      <Map />
+      {err ? <Text>Please enable location services</Text> : null}
+    </SafeAreaView>
   );
 };
+/**
+ *  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
 
+  import { requestForegroundPermissionsAsync } from 'expo-location';
+  const { granted } = await requestForegroundPermissionsAsync();
+ */
 export default TrackCreateScreen;
 
 const styles = StyleSheet.create({});
